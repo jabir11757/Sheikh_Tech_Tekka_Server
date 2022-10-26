9c6249a1a647bc965c9fc4b735db52762ce22365
jabir11757 <smjalsaba11757@gmail.com>
2022-10-26 23:07:41 +0600

    API is ready

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 5000;
+
+app.use(cors())
 
 
 const courses = require('./courses.json')
+const categories = require('./categories.json')
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
 app.get('/courses', (req, res) => {
-    res.send(courses)
+    res.send(courses);
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(c => c.id === id);
+    res.send(selectedCourse);
+})
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCategory = categories.find(category => category.category_id === id);
+    res.send(selectedCategory);
 })
 
 app.listen(port, () => {
